d1f6f90be65cf3e352d50cf22493b82809f0f49b
Artem Molokov <molokov.artem@gmail.com>
2018-03-30 13:48:42 +0300

    Provide basic config for the project

---
+++ webpack.config.babel.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+export default {
+  entry: {
+    app: path.resolve(__dirname, "client" "index.js")
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  devtool: "source-map",
+
+  watch: true,
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, "client"),
+      "node_modules"
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: "[local]--[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("postcss-css-reset")(),
+                require("autoprefixer")()
+              ]
+            }
+          },
+          "sass-loader"
+        ],
+        include: [
+          /src/
+        ]
+      }
+    ]
+  }
+};
